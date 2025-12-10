285d37aa554ba4e430c907bd8432a5058b3f3c03
Marcyline Guthrie <desertfruit.max@gmail.com>
2025-12-09 18:11:12 -0800

    finalized hello; made newyear

--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse('Im actually gonna do it')
+
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+        "name": name
+    })
