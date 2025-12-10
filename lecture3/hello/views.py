270034e0c832f807f94c142d92db69ab3355a04b
Marcyline Guthrie <desertfruit.max@gmail.com>
2025-12-09 23:40:55 -0800

    final update

--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Im actually gonna do it')
+    return HttpResponse('Goin funky mode')
 
 def greet(request, name):
     return render(request, "hello/greet.html", {
